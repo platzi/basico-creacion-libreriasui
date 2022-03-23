3d62d8ad0b30f925768d386ed70d81269141297d
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2022-03-23 12:10:58 -0500

    created button component

---
+++ src/Button/Button.js
@@ -0,0 +1,50 @@
+import { forwardRef } from 'react'
+import { cx, css } from '@emotion/css'
+import { PropTypes } from 'prop-types'
+
+const buttonStyles = (bgColor, color, width, height, bgColorHover, colorHover, borderRadius) => css`
+  background-color: ${bgColor};
+  color: ${color};
+  border-radius: ${borderRadius || "8px" };
+  width: ${width || "150px"};
+  height: ${height || "40px"};
+  text-align: center;
+  &:hover{
+    background-color: ${bgColorHover};
+    color: ${colorHover};
+  }
+`
+
+const Button = forwardRef((props, ref) => {
+  const { 
+    className,
+    bgColor,
+    color,
+    width,
+    height,
+    bgColorHover,
+    colorHover,
+    borderRadius} = props;
+  return(
+    <button
+      ref={ref}
+      type="button"
+      {...otherProps}
+      className={cx(buttonStyles(bgColor, color, width, height, bgColorHover, colorHover, borderRadius), className)}
+    />
+  )
+});
+
+Button.PropTypes= {
+  className: PropTypes.string,
+  bgColor: PropTypes.string,
+  color: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  bgColorHover: PropTypes.string,
+  colorHover: PropTypes.string,
+  borderRadius: PropTypes.string,
+
+}
+
+export default Button;
